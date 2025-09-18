cd813b81dbf35ea21f7e7797108368373f458b62
George Reyna <georgereyna@gmail.com>
2025-09-18 07:17:48 -0500

    6

--- operators/reisift_ops.py
+++
@@ -1,8 +0,0 @@
-def push_to_rei_sift(record):
-    """
-    Placeholder for sending a lead into REI SIFT.
-    record is a dictionary of all lead info.
-    For now, we just pretend it worked.
-    """
-    # TODO: later, add real steps to open REI SIFT and paste data
-    return True
--- operators/salesrabbit_ops.py
+++
@@ -1,8 +0,0 @@
-def create_salesrabbit_lead(record, assignee="Jason"):
-    """
-    Placeholder for creating a lead in SalesRabbit.
-    record holds all lead details; assignee is who gets the lead.
-    For now, we just pretend it worked.
-    """
-    # TODO: later, automate SalesRabbit form-filling here
-    return True
